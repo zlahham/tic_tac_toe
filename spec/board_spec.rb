dc2e6016cd0b682c054c2fd5217c165c8cb9311a
Zaid Al Lahham <zlahham@gmail.com>
2016-01-08 16:31:27 +0000

    new board returns correct grid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,15 +1,27 @@
 module TicTacToe
   describe Board do
-    subject { Board.new(grid: 'grid') }
+    subject     { Board.new(grid: 'grid') }
+    let(:board) { Board.new }
 
-    context "#initialize" do
-      it "initializes the board with a grid" do
+    context '#initialize' do
+      it 'initializes the board with a grid' do
         expect { subject }.not_to raise_error
       end
 
       it 'default grid has 3 arrays' do
-        board = Board.new
         expect(board.grid.length).to eq 3
+      end
+
+      it 'has 3 elements in each sub array' do
+        board.grid.each do |row|
+          expect(row.length).to eq 3
+        end
+      end
+    end
+
+    context '#grid' do
+      it 'outputs the grid' do
+        expect(subject.grid).to eq 'grid'
       end
     end
   end
