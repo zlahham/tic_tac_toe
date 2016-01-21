34bbfdce9db0b967c3809f7902ee899a21ad5a7e
Zaid Al Lahham <zlahham@gmail.com>
2016-01-21 16:58:57 +0000

    removed the old game

--- lib/game_modes/player_vs_computer.rb
+++ lib/game_modes/player_vs_computer.rb
@@ -20,11 +20,7 @@ module TicTacToe
         puts "\n\nWho would you like to go first? #{'Player 1'.blue}: #{player_1.name.blue} or #{'Player 2'.red}: #{player_2.name.red}"
         puts "Please make a choice using #{'1'.underline} or #{'2'.underline}".green
         choice = gets.chomp.to_i
-        if choice == 1
-          [player_1, player_2]
-        elsif choice == 2
-          [player_2, player_1]
-        end
+        choice == 1 ? [player_1, player_2] : [player_2, player_1]
       end
 
       def ask_name_for(player)
